ca7e868eaccdd4385963f9b2fd4d81dbd1d3e3b4
Savija pasandul <Pasanduls@gmail.com>
2024-04-08 23:24:26 +0100

    new

--- app/ui_components/login_ui.py
+++ app/ui_components/login_ui.py
@@ -1,5 +1,5 @@
 from flet import *
-from ui_components import UserInputField, SignInButton
+# from ui_components import UserInputField, SignInButton
 
 def login_ui(page: Page):
     page.horizontal_alignment = "center"

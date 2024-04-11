d596141669e916fecac68014e606c95e3a16a149
Savija pasandul <Pasanduls@gmail.com>
2024-04-11 10:27:09 +0100

    new

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 import flet
 from flet import *
-from ui_components.login_ui import LoginUI
-from ui_components.new_user_ui import NewUserUI
+# from ui_components.login_ui import LoginUI
+# from ui_components.new_user_ui import NewUserUI
 from ui_components.welcome_ui import WelcomeUI
-from authentication.authentication import handle_create_account
+# from authentication.authentication import handle_create_account
 
 def main(page: Page):
     page.horizontal_alignment = "center"
@@ -11,13 +11,13 @@ def main(page: Page):
     page.bgcolor = "#212328"
     
     # Create UI components instances
-    login_ui = LoginUI(page)
-    new_user_ui = NewUserUI(page)
+    # login_ui = LoginUI(page)
+    # new_user_ui = NewUserUI(page)
     welcome_ui = WelcomeUI(page)
 
     # Build UI components
-    login_ui.build()
-    new_user_ui.build()
+    # login_ui.build()
+    # new_user_ui.build()
     welcome_ui.build()
 
 if __name__ == "__main__":

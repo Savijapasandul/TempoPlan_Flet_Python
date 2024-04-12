5e0347f2b135d04e5dffc13db882ef9574f91a8d
Savija pasandul <Pasanduls@gmail.com>
2024-04-12 18:01:02 +0100

    new

--- mapp/ft_routes/dashboard.py
+++ mapp/ft_routes/dashboard.py
@@ -1,24 +1,10 @@
-# main.py
+from flet import *
 
-# modules
-from flet import flet, UserControl, Page
-#, Column, Row, Container, Text, padding, alignment
+class Dashboard:
+    def __init__(self, page: Page):
+        self.page = page
 
-# main class
-class Dashboard(UserControl):
     def build(self):
-        return Column(
-            controls=[]
-        )
-
-def start (page: Page):
-    page.title = 'TempoRoomPlan'
-    page.horizontal_alignment = 'Center'
-    page.vertical_alignment = 'Center'
-
-    app = Dashboard()
-    page.add(app)
-    page.update()
-
-if __name__ == "__name__":
-    flet.app(target=start)
+        # Define the UI components for the welcome screen
+        self.page.add(Text("Welcome"))
+        self.page.update()
--- mapp/main.py
+++ mapp/main.py
@@ -1,25 +1,20 @@
-# <main.py>
+# modules
 import flet
 from flet import *
-from ui_components.login_ui import LoginUI
-# from ui_components.new_user_ui import NewUserUI
-# from ui_components.welcome_ui import WelcomeUI
-# from authentication.authentication import handle_create_account
+from ft_routes.dashboard import Dashboard
 
 def main(page: Page):
+    page.title = 'TempoRoomPlan'
     page.horizontal_alignment = "center"
     page.vertical_alignment = "center"
-    page.bgcolor = "#212328"
+    page.bgcolor = "black"
+    page.update()
     
-    # Create UI components instances
-    login_ui = LoginUI(page)  
-    # new_user_ui = NewUserUI(page)
-    # welcome_ui = WelcomeUI(page)
+    # instances
+    dashboard = Dashboard(page)  
 
-    # Build UI components
-    login_ui.build()  
-    # new_user_ui.build()
-    # welcome_ui.build()
+    # Build components
+    dashboard.build()  
 
 if __name__ == "__main__":
     flet.app(target=main)

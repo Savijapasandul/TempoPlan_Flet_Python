2543f1fec1cefc4a5ee1c462c4a287f68a525a07
Savija pasandul <Pasanduls@gmail.com>
2024-04-13 11:31:39 +0100

    basic layout complete

--- mapp/main.py
+++ mapp/main.py
@@ -1,12 +1,16 @@
+# modules
 import flet as ft
 
+# main page
 def main(page):
 
+    # page settings
     page.adaptive = True
     page.theme_mode = ft.ThemeMode.LIGHT
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.vertical_alignment = ft.CrossAxisAlignment.CENTER
     
+    # light theme / dark theme
     def toggle_theme_button(e):
         if e.control.selected:
             page.theme_mode = ft.ThemeMode.LIGHT
@@ -17,10 +21,26 @@ def main(page):
         e.control.selected = not e.control.selected
         e.control.update()
 
+    # bottom bar
+    bottom_app_bar = ft.BottomAppBar(
+        bgcolor=ft.colors.SURFACE_VARIANT,
+        shape=ft.NotchShape.CIRCULAR,
+        content=ft.Row(
+            controls=[
+                ft.IconButton(icon=ft.icons.CALENDAR_MONTH, style=ft.ButtonStyle(padding=0), on_click=lambda _: page.go("/calendar")),
+                ft.IconButton(icon=ft.icons.HOME, style=ft.ButtonStyle(padding=0), on_click=lambda _: page.go("/")),
+                ft.IconButton(icon=ft.icons.SETTINGS, style=ft.ButtonStyle(padding=0), on_click=lambda _: page.go("/settings")),
+            ],
+        )
+    )
+
+    # route handler
     def route_change(route):
-        page.views.clear()
+    
+        page.views.clear()    
         page.views.append(
             ft.View(
+                # home page
                 "/",
                 [
                     ft.AppBar(
@@ -36,43 +56,25 @@ def main(page):
                             )
                         ],
                     ),
-                    ft.BottomAppBar(
-                        bgcolor=ft.colors.GREY,
-                        shape=ft.NotchShape.CIRCULAR,
-                        content=ft.Row(
-                            controls=[
-                                ft.IconButton(icon=ft.icons.CALENDAR_MONTH, on_click=lambda _: page.go("/calendar")),
-                                ft.IconButton(icon=ft.icons.HOME, on_click=lambda _: page.go("/")),
-                                ft.IconButton(icon=ft.icons.SETTINGS, on_click=lambda _: page.go("/settings")),
-                            ],
-                        )
-                    )
+                    bottom_app_bar
                 ],
             )
         )
         if page.route == "/calendar":
             page.views.append(
                 ft.View(
+                    # calendar page
                     "/calendar",
                     [
                         ft.AppBar(title=ft.Text("Calendar"), bgcolor=ft.colors.SURFACE_VARIANT),
-                        ft.BottomAppBar(
-                        bgcolor=ft.colors.GREY,
-                        shape=ft.NotchShape.CIRCULAR,
-                        content=ft.Row(
-                            controls=[
-                                ft.IconButton(icon=ft.icons.CALENDAR_MONTH, on_click=lambda _: page.go("/calendar")),
-                                ft.IconButton(icon=ft.icons.HOME, on_click=lambda _: page.go("/")),
-                                ft.IconButton(icon=ft.icons.SETTINGS, on_click=lambda _: page.go("/settings")),
-                            ],
-                        )
-                    )
+                        bottom_app_bar
                     ],
                 )
             )
         if page.route == "/settings":
             page.views.append(
                 ft.View(
+                    # setting page
                     "/settings",
                     [
                         ft.AppBar(
@@ -80,7 +82,7 @@ def main(page):
                             bgcolor=ft.colors.SURFACE_VARIANT,
                             actions=[
                                 ft.IconButton(
-                                    icon=ft.icons.LIGHT_MODE, 
+                                    icon=ft.icons.LIGHT_MODE,
                                     selected_icon=ft.icons.DARK_MODE,
                                     style=ft.ButtonStyle(padding=0),
                                     selected=False,
@@ -88,22 +90,13 @@ def main(page):
                                 ),
                             ],
                         ),
-                        ft.BottomAppBar(
-                        bgcolor=ft.colors.GREY,
-                        shape=ft.NotchShape.CIRCULAR,
-                        content=ft.Row(
-                            controls=[
-                                ft.IconButton(icon=ft.icons.CALENDAR_MONTH, on_click=lambda _: page.go("/calendar")),
-                                ft.IconButton(icon=ft.icons.HOME, on_click=lambda _: page.go("/")),
-                                ft.IconButton(icon=ft.icons.SETTINGS, on_click=lambda _: page.go("/settings")),
-                            ],
-                        )
-                    )
+                        bottom_app_bar
                     ],
                 )
             )
         page.update()
 
+    # page view
     def view_pop(view):
         page.views.pop()
         top_view = page.views[-1]
@@ -112,9 +105,5 @@ def main(page):
     page.on_route_change = route_change
     page.on_view_pop = view_pop
     page.go(page.route)
-
-    page.add(ft.Text(f"Initial route: {page.route}"))
-
-    page.add(ft.SafeArea(ft.Text("Hello")))
 
 ft.app(target=main)
